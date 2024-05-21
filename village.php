0161676241c38e407b69be2e98622ddddbf8861b
Lucas <165795820+LucasB67@users.noreply.github.com>
2024-05-21 16:08:09 +0200

    Create village.php

---
+++ village.php
@@ -0,0 +1,35 @@
+<?php
+
+// On se connecte à la base.
+try {
+    $mysqlClient = new PDO('mysql:host=localhost;dbname=gaulois;charset=utf8', 'root', '');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+$id = 1;
+$sqlQuery = "SELECT * 
+                FROM lieu 
+                WHERE id_lieu = :id";
+$villageStatement = $mysqlClient->prepare($sqlQuery);
+$villageStatement->execute(["id" => $id]);
+$village = $villageStatement->fetch();
+
+
+$sqlQuery = "SELECT * 
+                FROM personnage
+                WHERE id_lieu = :id";
+$personnagesStatement = $mysqlClient->prepare($sqlQuery);
+$personnagesStatement->execute(["id" => $id]);
+$personnages = $personnagesStatement->fetchAll();
+
+
+echo "<h1>".$village['nom_lieu']."</h1><ul>";
+foreach ($personnages as $personnage) {
+
+    echo "<li>".$personnage['nom_personnage']."</li>";
+    }
+echo "</ul>"
+
+?>
